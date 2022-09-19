e31a9cfb2ad24f570f478763f56291b4a1c1cfc8
bataviel1997 <luqmansa66@gmail.com>
2022-09-19 23:28:28 +0700

    upload 19 september 2022

---
+++ src/pages/404.js
@@ -0,0 +1,5 @@
+const app = document.getElementById('app')
+
+export default function not_found(){
+
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,29 @@
+const app = document.getElementById('app')
+
+export default function Home(){
+    app.innerHTML = `
+    <div class="auth">
+        
+        <from class="auth-from">
+
+        <h1>Login Page</h1>
+            <div class="from-group">
+                <label>Email</label>
+                <input type="email" id="email" required/>
+            </div>
+
+            <div class="from-group">
+                <label>Password</label>
+                <input type="password" id="password" required/>
+            </div>
+
+            <button type="submit">Login</button>
+        
+        
+        </from>
+
+
+    </div>
+    `
+
+}
